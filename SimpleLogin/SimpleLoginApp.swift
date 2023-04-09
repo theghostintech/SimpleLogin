24a1d64b603f42a562d05c325364ca9f8fd9b05f
theghostintech <theghostintech@gmail.com>
2023-04-09 10:53:00 -0700

    Initial Commit

---
+++ SimpleLogin/SimpleLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLoginApp.swift
+//  SimpleLogin
+//
+//  Created by Joshua Birdine on 4/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
